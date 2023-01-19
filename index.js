e4a23e49859dd9677a109bf219c12d9e88b83af6
Jacob Nollen <jacob.nollen@netplanner.com>
2023-01-19 12:04:37 -0500

    finished initial code and testing readme file

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 // TODO: Include packages needed for this application
-import inquirer from 'inquirer';
-const fs = require('FS');
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 // TODO: Create an array of questions for user input
 const questions = [
-    "What is the Title of your project?",
-    "What is the Description of your project?",
-    "What are the installation instructions for your project?",
-    "What is your project Used for?",
-    "How can someone else contribute to your project?",
-    "How can someone test your project?",
+    {
+        name:"title",
+        mesage: "What is the Title of your project?",
+    },
+    {
+        name:"desc",
+        message:"What is the Description of your project?",
+    },
+    {
+        name:"install",
+        message: "What are the installation instructions for your project?",
+    },
+    {
+        name:"use",
+        message:"What is your project used for?",
+    },
+    {
+        name:"contrib",
+        message:"How can someone else contribute to your project?",
+    },
+    {
+        name:"test",
+        message:"How can someone test your project?",
+    }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(`${fileName}.md`, data, (err) => {
+    fs.writeFile(`${fileName}-Gen.md`, data, (err) => {
         if (err) {
             console.log(`File write was not successful: ${err}`);
         }
@@ -27,7 +46,21 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions)
         .then(answers => {
-            console.log(answers);
+            let fileData = `
+            # ${answers.title}\n
+            ## Description
+            ${answers.desc}\n
+            ## Installation
+            ${answers.install}\n
+            ## Usage
+            ${answers.use}\n
+            ## Contributing
+            ${answers.contrib}\n
+            ## Tests
+            ${answers.test}\n
+            `;
+
+            writeToFile("ReadME", fileData);
         })
         .catch(err => {
             if (err.isTtyError) {
